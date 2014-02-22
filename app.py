35a674ce4fb8744ee90b8c7a1304cdf8eebe93fa
florinpapa <florin.papa192@gmail.com>
2014-02-22 14:14:27 +0200

    add unit code

--- app.py
+++ app.py
@@ -4,7 +4,34 @@ import pickle
 import csv
 
 
-app = Flask(__name__,static_url_path='/static')
+app = Flask(__name__, static_url_path='/static')
+
+
+#afisare informatii in functie de codul entitatii
+@app.route("/muzee/<code>")
+def get_museum_by_code(code):
+    #load dictionary
+    dict_file = open('data.pkl', 'rb')
+    dictionar = pickle.load(dict_file)
+    dict_file.close()
+    #load headers
+    head_file = open('headers.hd', 'rb')
+    header = pickle.load(head_file)
+    head_file.close()
+    #cautare cod
+    try:
+        index = dictionar[header[0]].index(code)
+        new_d = {'judet': dictionar[header[2]][index].decode(encoding="UTF-8"),
+                 'de_ro': dictionar[header[3]][index].decode(encoding="UTF-8"),
+                 'de_en': dictionar[header[4]][index].decode(encoding="UTF-8"),
+                 'loc': dictionar[header[5]][index].decode(encoding="UTF-8"),
+                 'adr': dictionar[header[7]][index].decode(encoding="UTF-8"),
+                 'tel': dictionar[header[9]][index].decode(encoding="UTF-8"),
+                 'p_ro': dictionar[header[12]][index].decode(encoding="UTF-8"),
+                 'p_en': dictionar[header[13]][index].decode(encoding="UTF-8")}
+        return render_template('muzeu.html', info=new_d)
+    except:
+        return "Nu s-au gasit potriviri"
 
 
 @app.route("/")
@@ -34,7 +61,14 @@ def getCSV():
                 for i in range(len(content_list)):
                     dictionar[header[i]].append(content_list[i])
             total += content
+    output = open('data.pkl', 'wb')
+    pickle.dump(dictionar, output)
+    output.close()
+    headers = open('headers.hd', 'wb')
+    pickle.dump(header, headers)
+    headers.close()
     return "|".join(dictionar[header[3]])
+
 
 @app.route('/muzee')
 def toateMuzeele():
@@ -45,7 +79,9 @@ def toateMuzeele():
     data = pickle.load(open('data.pkl', 'rb'))
     muzee = []
     for i in range(len(data[header[0]])):
-        muzee.append({'cod': data[header[0]][i], 'judet': data[header[2]][i].decode(encoding="UTF-8"), 'nume': data[header[3]][i].decode(encoding="UTF-8")})
+        muzee.append({'cod': data[header[0]][i],
+                     'judet': data[header[2]][i].decode(encoding="UTF-8"),
+                     'nume': data[header[3]][i].decode(encoding="UTF-8")})
     return render_template('lista_muzee.html', muzee=muzee)
 
 if __name__ == "__main__":
